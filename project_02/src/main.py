40a45ab18d13512fe2b1289fdb51419c9d3ce33a
Stewart Schuler <>
2025-09-24 19:22:03 -0400

    Added complete code

--- project_02/src/main.py
+++ project_02/src/main.py
@@ -84,14 +84,16 @@ x_val   = ss_x.transform(x_val  )
 #          Part C
 # ----------------------------------
 
-lr = 0.001;
+lr = 0.01;
+n_epochs= 500;
+
 b = np.random.randn(1);
 w = np.random.randn(1);
-for epoch in range(5000):
+for epoch in range(n_epochs):
     y_hat = b + w * x_train;
     err = (y_hat - y_train);
     loss = (err**2).mean();
-    print(f"[{epoch}] b={b}    w={w}    Loss={loss}");
+    #print(f"[{epoch}] b={b}    w={w}    Loss={loss}");
     
     b_grad = 2*err.mean();
     w_grad = 2*(x_train*err).mean();
@@ -106,7 +108,7 @@ for epoch in range(5000):
 y_pred = b + w * x_val;
 err = (y_pred - y_val);
 loss = (err**2).mean();
-print(f"Fianl b={b} w={w} loss={loss}")
+print(f"Fianl b={b[0]:10.6f} w={w[0]:10.6f} loss={loss:.7f}")
 
 # ----------------------------------
 #          Part E
@@ -126,3 +128,51 @@ plt.yscale("log");
 plt.plot( x_lr, np.e**y_lr, '--', color='black', label='Linear Regression', linewidth=2 );
 plt.legend();
 #plt.scatter(x_train, y_train );
+
+print(f"Manual LR:\n  Normalized:   b: {b[0]:10.6f} w:{w[0]:10.6f}\n  Unnormalized: b: {b_plt[0]:10.6f} w: {w_plt[0]:10.6f}\n")
+
+# ----------------------------------
+#          Part F
+# ----------------------------------
+
+import numpy as np
+import torch
+import torch.optim as optim
+import torch.nn as nn
+
+class LR_Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        torch.manual_seed(1140)
+        self.b = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
+        self.w = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
+    def forward(self, x):
+        return self.b + self.w * x
+    
+
+x_train_tensor = torch.as_tensor(x_train).float()
+y_train_tensor = torch.as_tensor(y_train).float()
+
+model = LR_Model()
+optimizer=optim.SGD(model.parameters(), lr=lr)
+loss_fn=nn.MSELoss(reduction='mean')
+
+for epoch in range(n_epochs):
+    
+    model.train()
+    yhat = model(x_train_tensor)
+    loss = loss_fn(yhat, y_train_tensor)
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+P=list(model.parameters())
+bt=float(P[0].detach())
+wt=float(P[1].detach())
+yt_pred = model(torch.as_tensor(x_val).float());
+losst = loss_fn(yt_pred, torch.as_tensor(y_val).float())
+print(f"Fianl b={bt:10.6f} w={wt:10.6f} loss={losst:.7f}")
+
+wt_plt = wt / sigma;
+bt_plt = bt - (w * mu / sigma);
+print(f"Torch LR:\n  Normalized:   b: {bt:10.6f} w:{wt:10.6f}\n  Unnormalized: b: {bt_plt[0]:10.6f} w: {wt_plt:10.6f}")
